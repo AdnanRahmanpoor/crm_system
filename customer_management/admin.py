37e5022303c7fef144bb46918b083a801db0c6ce
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-22 23:38:40 +0400

    Create superuser and setup admin

--- customer_management/admin.py
+++ customer_management/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Customer, Project
 
-# Register your models here.
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 1
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'company')
+    search_fields = ('name', 'email')
+    inlines = [ProjectInline]
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'customer', 'status', 'start_date', 'end_date')
+    list_filter = ('status', 'start_date', 'end_date')
+    search_fields = ('title', 'customer__name')
