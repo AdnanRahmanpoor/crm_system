c6be283ecfc32a83844baa80c72f0a1b800a4fa9
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-24 23:03:32 +0400

    Changed styling to TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/templates/customer_management/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
